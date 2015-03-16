7e7e53ba82334e640840e2d4df76be658248e7d1
Administrator <administrator@ubuntu.(none)>
2015-03-16 19:48:48 +0100

    paypal integration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Course.delete_all
-Course.create name: "INTRODUCTION TO REPOSTERY (4 Weeks)", price: 475
-Course.create name: "THE BEST BUTCHERY COURSE (4 Weeks)", price: 610
-Course.create name: "SEA FOOD SECRETS COURSE (8 Weeks)", price: 1485
+Course.create name: "Course1 : $475", price: 475
+Course.create name: "Course2 : $610", price: 610
+Course.create name: "Course3 : $1485", price: 1485
